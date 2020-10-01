5f159688d7360799f2483816cf6774ec4b6e5122
popoway <31576265+popoway@users.noreply.github.com>
2020-09-30 23:10:27 -0400

    fix: broken js with Webpack

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,21 @@
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+var bodyParser = require('body-parser')
+var cors = require('cors')
+const dotenv = require('dotenv');
+dotenv.config();
+
+var textapikey = process.env.API_KEY;
 
 const app = express()
+app.use(cors())
+// to use json
+app.use(bodyParser.json())
+// to use url encoded values
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
 app.use(express.static('dist'))
 
@@ -14,8 +27,8 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 })
 
 app.get('/test', function (req, res) {
